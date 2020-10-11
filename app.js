796f5b5ecbc17ed9539507a9e0a30a53e5d52c72
baptiste lhopitault <lhopitault.baptiste@gmail.com>
2020-10-11 23:12:41 +0200

    Show chart of number of storm by years

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let file_to_read = 'tempetes.json' 
+
  
  function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
@@ -16,18 +17,100 @@ let file_to_read = 'tempetes.json'
 
 
 
+function get_array_of_years(starting_year, ending_year){
+    let start_date = new Date(starting_year)
+    let end_date = new Date(ending_year)
+    array_of_years = []
+    let dt = new Date(starting_year);
+   
+     while (dt <= new Date(ending_year)) {
+        array_of_years.push(new Date(dt).getFullYear());
+        dt.setFullYear(dt.getFullYear() + 1);
+    }
+
+
+
+    return array_of_years
+
+}
+
+
+function groupBy(objectArray, property) {
+    return objectArray.reduce((acc, obj) => {
+       const key = obj[property];
+       if (!acc[key]) {
+          acc[key] = [];
+       }
+       // Add object to list for given key's value
+       acc[key].push(obj);
+       return acc;
+    }, {});
+ }
+
+
+function get_number_in_array(value, arr){
+    
+
+}
+
+
 
 function init() {
     loadJSON(function(response) {
-     // Parsing JSON string into object
-       let tempetes = JSON.parse(response);
-       for (const tempete of tempetes){
-           console.log(tempete.Date.split('/')[0]);
-       }
+        // Parsing JSON string into object
+        let tempetes = JSON.parse(response);
+        let tempetes_date_parsed = []
+        let array_of_years = get_array_of_years('1987', '2020')
+      
+        for (const tempete of tempetes){
+                tempetes_date_parsed.push({Name:tempete.Name, Date: new Date(tempete.Date).getFullYear(), ressource:tempete.Ressource})           
+        }
+
+        let tempetes_number_by_year = groupBy(tempetes_date_parsed, 'Date')
+
+        let tempetes_number_by_year_filled = array_of_years.map( year =>{
+            return tempetes_number_by_year[year] 
+        })
+
+        let datas = []
+        for (const year of tempetes_number_by_year_filled) {
+            if(year !== undefined){
+                datas.push(year.length)
+                
+            }else {
+                datas.push(0)
+            }
+        }
+
+       
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: array_of_years,
+                datasets: [{
+                    label: 'Nombre de tempètes',
+                    data: datas,
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+
+       console.log(datas);
     });
 }
 
 
 
 init();
+
 
